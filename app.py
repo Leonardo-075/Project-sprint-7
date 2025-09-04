a1c485b3be42d1071f18f5c906d301b37fdcc1d7
Leonardo <cardozo.edison@gmail.com>
2025-09-04 09:34:44 -0500

    Agregar tabla con los datos.

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ import streamlit as st
 st.header('Visualización de datos de anuncios de venta de coches')
 
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
-hist_button = st.button('Construir histograma')  # crear un botón
+
+# Crear tabla de datos
+st.dataframe(car_data)
+
+# crear un botón para el histograma.
+hist_button = st.button('Construir histograma')
 
 if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
@@ -13,12 +18,20 @@ if hist_button:  # al hacer clic en el botón
         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 
     # crear un histograma
-    fig = px.histogram(car_data, x='odometer', title='Histograma del odómetro')
+    fig = px.histogram(car_data, x='odometer',
+                       title='Histograma del kilometraje(millas) de los coches')
+    # agregar etiquetas
+    fig.update_layout(
+        xaxis_title='Kilometraje (millas)',
+        yaxis_title='Cantidad de coches',
+        bargap=0.2,  # espacio entre barras
+    )
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True, theme="streamlit")
 
-disp_button = st.button('Construir diagrama de dispersión')  # crear un botón
+# crear un botón para el diagrama de dispersión.
+disp_button = st.button('Construir diagrama de dispersión')
 if disp_button:  # al hacer clic en el botón
     # escribir un mensaje
     st.write(
@@ -26,7 +39,7 @@ if disp_button:  # al hacer clic en el botón
 
     # crear un diagrama de dispersión
     fig = px.scatter(car_data, x='model_year', y='price',
-                     title='Diagrama de dispersión del precio frente al año')
+                     title='Diagrama de dispersión del modelo respecto al precio')
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True, theme="streamlit")
